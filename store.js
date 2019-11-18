05c9d5adb5bb65e2c6524017cc116c0cb39b0b42
Nik Fedoseev <nikfedoseev@MacBook-Pro-Nik.local>
2019-11-18 22:11:58 +0300

    ololo

--- store.js
+++ store.js
@@ -3,16 +3,31 @@ const createStore = () => {
   return {
     addItem: function({group, name, salary}) {
       if(!store[group]) {
-        return store[group] = []
+        store[group] = []
+        store[group].push({name, salary})
+        
       }
-      return store[group].push({name, salary})
+      store[group].push({name, salary})
     },
     removeItem: function({group, name}) {
       store = store[group].filter(item => item[name] !== name)
     },
+    getStore: function() {
+      return store
+    }
+  }
+}
+
+const makeEployee = (group, name, salary) => {
+  return {
+    group, name, salary
   }
 }
 
 
 const userDataBase = createStore()
-userDataBase.addItem()
+const user = makeEployee('programming', "olol", 300000)
+userDataBase.addItem(Nik)
+
+console.log(userDataBase.getStore())
+// userDataBase.addItem()
