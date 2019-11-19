7ad90ff827af1a3c88affc4c76dfc2e956dc6b01
Nikita Fedoseev <n.fedoseev@onpoint.ru>
2019-11-19 08:49:27 +0300

    fix

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ import {
 
 const userDataBase = createStore();
 
-const employee = makeEmployee("programming", "employee", 300000);
-const programmer = makeProgrammer("Vasya", 100);
+// const employee = makeEmployee("programming", "employee", 300000);
+// const programmer = makeProgrammer("Vasya", 100);
 
-userDataBase.addItem(employee);
-userDataBase.removeItem(employee);
-userDataBase.addItem(programmer);
+// userDataBase.addItem(employee);
+// userDataBase.addItem(programmer);
 
-const vasya = userDataBase.getItem("programming", "Vasya");
-const vasyaSalary = getSalary(vasya);
-const vasyaSalaryAfterNDS = getSalaryAfterNDS(vasya);
-
-console.log(vasyaSalary, vasyaSalaryAfterNDS);
+// const vasya = userDataBase.getItem("programming", "Vasya");
+// const vasyaSalary = getSalary(vasya);
+// const vasyaSalaryAfterNDS = getSalaryAfterNDS(vasya);
