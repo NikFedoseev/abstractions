79b7fda1c9e7c4519d64d89ea8c1be721e89a123
Nik Fedoseev <nikfedoseev@MacBook-Pro-Nik.local>
2019-11-24 21:08:08 +0300

    fix

--- abstractions.js
+++ abstractions.js
@@ -1,42 +1,59 @@
-export const createStore = () => {
-  let store = {};
+const suqare = (v) => Math.pow(v, 2);
+const cube = (v) => Math.pow(v,3);
+
+console.log(suqare(5))
+console.log(cube(5))
+
+const forEach = (arr, cb) => {
+  for(let i = 0; i < arr.length; i++) {
+    cb(arr[i], i)
+  }
+}
+
+{
+  const arr = [1, 2, 3, 4, 5]
+  forEach(arr, (el, i) => console.log(el, i))
+}
+
+const filter = (arr, cb) => {
+  let res = [];
+  for(let i = 0; i < arr.length; i++) {
+    cb(arr[i], i) && res.push(arr[i])
+  }
+  return res
+}
+
+const greaterThen = (n) => (v) => v > n;
+const greaterThen3 = greaterThen(3)
+
+{
+  const arr = [1, 2, 3, 4, 5];
+  const filtered = filter(arr, greaterThen3);
+  console.log(filtered)
+}
+
+function sum (arr) {
+  let res = 0;
+  forEach(arr, el => res = res + el)
+  return res;
+}
+
+{
+  const arr = [1, 2, 3, 4, 5];
+  const res = sum(filter(arr, greaterThen3))
+  console.log(res)
+}
+
+const makeStack = () => {
+  const stack = [];
   return {
-    addItem: function({ group, ...properties }) {
-      if (!store[group]) {
-        store[group] = [];
-      }
-      store[group].push(properties);
-    },
-    removeItem: function({ group, name }) {
-      store[group] = store[group].filter(employee => employee.name !== name);
-    },
-    getItem: function(group, name) {
-      return store[group].filter(employee => employee.name == name)[0];
-    },
-    getStore: function() {
-      return store;
-    }
-  };
-};
+    add: (el) => stack.push(el),
+    get: () => stack.pop(el),
+    getStack: () => stack
+  }
+}
 
-export const getExpenses = db => {
-  const store = db.getStore();
-  return Object.values(store)[0].reduce((sum, el) => {
-    return sum + el.salary;
-  }, 0);
-};
+const stack = makeStack();
+stack.add(1)
+console.log(stack.getStack())
 
-export const getExpensesPerYear = db => getExpenses(db) * 12;
-
-export const makeEmployee = (group, name, salary) => ({
-  group,
-  name,
-  salary
-});
-
-export const makeProgrammer = (name, salary) =>
-  makeEmployee("programming", name, salary);
-
-export const getSalary = employee => employee.salary;
-
-export const getSalaryAfterNDS = employee => employee.salary * 0.87;
